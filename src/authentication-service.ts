77d164ed8d29d1ce80bff9be13c9b386f248e4fd
Antoine Cheron <cheron.antoine@gmail.com>
2020-04-11 11:13:34 +0200

    Add a very basic authentication service to manage bearer tokens

---
+++ src/authentication-service.ts
@@ -0,0 +1,34 @@
+const tokenLocalStorageKey = 'Authorization'
+
+class AuthenticationService {
+  constructor () {}
+
+  static isAuthenticated = () =>
+    AuthenticationService.getToken() !== undefined &&
+    AuthenticationService.getToken() !== null
+
+  static getToken () {
+    return window.localStorage.getItem(tokenLocalStorageKey)
+  }
+
+  static updateToken (token: string) {
+    window.localStorage.setItem(tokenLocalStorageKey, token)
+  }
+
+  static removeToken () {
+    window.localStorage.removeItem(tokenLocalStorageKey)
+  }
+
+  static currentTokenWasRefusedByApi () {
+    AuthenticationService.removeToken()
+  }
+}
+
+export default AuthenticationService
+/* 
+  TODO: create one kind of auth service per kind of auth mecanism, 
+  enable to extend this list and instantiate the proper service
+  based on one of the mechanism that the API supports, which can be
+  discovered in the documentation of the API that we retrieve when 
+  this app initialize
+*/
