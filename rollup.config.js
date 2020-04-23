55ab7705fa26dd55fe669aeacfc4616e7059b99a
Antoine Cheron <cheron.antoine@gmail.com>
2020-04-23 14:11:43 +0200

    Update rollup config to reduce the package size from 1.1MB to 380KB

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
 import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 
 import pkg from './package.json'
 
@@ -12,16 +10,14 @@ export default {
   output: [
     {
       file: pkg.main,
-      format: 'es',
-      sourcemap: true
+      format: 'es'
     }
   ],
+  external: ['axios', 'openapi-types'],
   plugins: [
-    external(),
     json(),
     typescript({ exclude: '/test/**', useTsconfigDeclarationDir: true }),
     commonjs(),
-    nodeResolve({ browser: true, preferBuiltins: true }),
-    sourceMaps()
+    nodeResolve({ browser: true, preferBuiltins: true })
   ]
 }
